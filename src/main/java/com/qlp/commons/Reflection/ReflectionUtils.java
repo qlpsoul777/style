f58cd1c87b0b7ffa5670b439c918e0901fe18b10
qulinpeng <755146647@qq.com>
2014-05-26 16:49:09 +0800

    增加反射方法工具类，新增两个待实现的方法（获取指定对象指定属性的值；往指定对象的指定属性注入指定值）

---
+++ src/main/java/com/qlp/commons/Reflection/ReflectionUtils.java
@@ -0,0 +1,33 @@
+package com.qlp.commons.Reflection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by qlp on 2014/5/26.
+ */
+public class ReflectionUtils {
+    private static Logger logger = LoggerFactory.getLogger(ReflectionUtils.class);
+
+    /**
+     * 获取指定对象指定属性名的值
+     *
+     * @param obj
+     * @param fieldName
+     * @return
+     */
+    public static Object getFieldValue(Object obj, String fieldName) {
+        return null;
+    }
+
+    /**
+     * 将指定对象的指定属性的值设置为指定值
+     *
+     * @param obj
+     * @param fieldName
+     * @param fieldValue
+     */
+    public static void setFieldValue(Object obj, String fieldName, Object fieldValue) {
+
+    }
+}
