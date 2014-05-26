922da9bb5bd8ff122a09b9c83f0b150c8212b2d4
qlpsoul777 <755146647@qq.com>
2014-05-26 12:50:47 +0800

    增加public abstract List<T> queryByMap(Map<String,Object> map,Sort sort);       public abstract List<T> queryByCriteria(Criteria criteria);       public abstract List<T> queryByCriteria(Criteria criteria,Sort sort);       public abstract Page<T> queryPageByMap(Map<String,Object> map,Pageable pageable);       public abstract Page<T> queryPageByCriteria(Criteria criteria,Sort sort);方法的申明

--- src/main/java/com/qlp/commons/orm/QlpJpaRepository.java
+++ src/main/java/com/qlp/commons/orm/QlpJpaRepository.java
@@ -1,7 +1,9 @@
 package com.qlp.commons.orm;
 
+import org.hibernate.Criteria;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
@@ -17,5 +19,13 @@ import java.util.Map;
 public abstract interface QlpJpaRepository<T,ID extends Serializable> extends JpaRepository<T,ID>{
     public abstract List<T> queryByMap(Map<String, Object> map);
 
+    public abstract List<T> queryByMap(Map<String, Object> map, Sort sort);
+
+    public abstract List<T> queryByCriteria(Criteria criteria);
+
+    public abstract List<T> queryByCriteria(Criteria criteria, Sort sort);
+
     public abstract Page<T> queryPageByMap(Map<String,Object> map,Pageable pageable);
+
+    public abstract Page<T> queryPageByCriteria(Criteria criteria, Sort sort);
 }
