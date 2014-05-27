7df847026749a1658cd5c208629d6f37118d32b9
qulinpeng <755146647@qq.com>
2014-05-27 17:02:31 +0800

    前台页面添加bootstrap2的支持

--- src/main/java/com/qlp/user/controller/UserController.java
+++ src/main/java/com/qlp/user/controller/UserController.java
@@ -9,13 +9,18 @@ import com.qlp.user.service.UserService;
 import com.qlp.utils.ParameterUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefaults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qlp on 14-4-15.
@@ -43,39 +48,56 @@ public class UserController {
         this.functionService = functionService;
     }
 
-    @RequestMapping(value = "first" , method = RequestMethod.GET)
-    public String first(){
+    @RequestMapping(value = "first", method = RequestMethod.GET)
+    public String first() {
         return "/style/user/index";
     }
 
     /**
      * 登录
-     * @param request  client request
+     *
+     * @param request client request
      * @param model   service response
      * @return url
      */
-    @RequestMapping(value = "login" , method = RequestMethod.POST)
-    public String login(HttpServletRequest request,Model model){
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(HttpServletRequest request, Model model) {
         String loginName = request.getParameter("loginName");
         String inputPassword = request.getParameter("password");
-        User user = userService.findByLoginNameAndPassword(loginName,inputPassword);
+        User user = userService.findByLoginNameAndPassword(loginName, inputPassword);
         String path = "/style/user/index";
         String errorMessage = "";  //登录失败提示信息
-        if(user != null){
-            if(StringUtils.equals(user.getState(),ParameterUtils.ENABLE)){
+        if (user != null) {
+            if (StringUtils.equals(user.getState(), ParameterUtils.ENABLE)) {
                 List<Application> apps = applicationService.findApplicationByUser(user.getId());
                 List<Functions> funcs = functionService.findFunctionsByUser(user.getId());
                 model.addAttribute("funcs", funcs);
                 model.addAttribute("apps", apps);
-                model.addAttribute("user",user);
+                model.addAttribute("user", user);
                 path = "/style/user/hello";
-            }else{
+            } else {
                 errorMessage = "账户已禁用，如有疑问请联系管理员";
             }
-        }else{
+        } else {
             errorMessage = "用户名或密码错误，请重试";
         }
-        model.addAttribute("msg",errorMessage);
+        model.addAttribute("msg", errorMessage);
         return path;
+    }
+
+    /**
+     * 查询用户列表
+     *
+     * @param request
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "list/{type}", method = RequestMethod.GET)
+    public String list(HttpServletRequest request, @PageableDefaults(10) Pageable pageable,
+                       @PathVariable("type") String type, Model model) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        String userName = request.getParameter("userName");  //用户名
+        String roleName = request.getParameter("roleName");  //角色名
+        return "/style/user/index";
     }
 }
--- src/main/webapp/WEB-INF/views/style/user/hello.jsp
+++ src/main/webapp/WEB-INF/views/style/user/hello.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <c:set var="ctx" value="${pageContext.request.contextPath}"/>
@@ -9,42 +9,52 @@
 <head>
     <title>注册页面</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="${ctx}/static/css/bootstrap.css">
+    <link rel="stylesheet" href="${ctx}/static/css/bootstrap.css" media="screen">
     <script src="${ctx}/static/js/jquery.js"></script>
     <script src="${ctx}/static/js/bootstrap.js"></script>
 </head>
 <body>
-<h1>恭喜您: ${user.loginName}，登录成功</h1>
-<table>
-    <thead>
-    <th>模块id</th>
-    <th>模块名</th>
-    </thead>
-    <tbody>
-    <c:forEach items="${apps}" var="a">
-        <tr>
-            <td>${a.id}</td>
-            <td>${a.name}</td>
-        </tr>
-    </c:forEach>
-    </tbody>
-</table>
-<table>
-    <thead>
-    <th>功能id</th>
-    <th>功能名</th>
-    <th>模块名</th>
-    </thead>
-    <tbody>
-    <c:forEach items="${funcs}" var="f">
-        <tr>
-            <td>${f.id}</td>
-            <td>${f.name}</td>
-            <td>${f.application.name}</td>
-        </tr>
-    </c:forEach>
-    </tbody>
-</table>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span2"></div>
+        <div class="span8">
+            <h1>恭喜您: ${user.loginName}，登录成功</h1>
+            <table class="table">
+                <thead>
+                <th>模块id</th>
+                <th>模块名</th>
+                </thead>
+                <tbody>
+                <c:forEach items="${apps}" var="a">
+                    <tr>
+                        <td>${a.id}</td>
+                        <td>${a.name}</td>
+                    </tr>
+                </c:forEach>
+                </tbody>
+            </table>
+            <table class="table">
+                <thead>
+                <th>功能id</th>
+                <th>功能名</th>
+                <th>模块名</th>
+                <th>操作</th>
+                </thead>
+                <tbody>
+                <c:forEach items="${funcs}" var="f">
+                    <tr>
+                        <td>${f.id}</td>
+                        <td>${f.name}</td>
+                        <td>${f.application.name}</td>
+                        <td><a href="${ctx}/${f.uri}">管理</a></td>
+                    </tr>
+                </c:forEach>
+                </tbody>
+            </table>
+        </div>
+        <div class="span2"></div>
+    </div>
+</div>
 </body>
 </html>
 
