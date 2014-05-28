1b8bd40cadea6635acc4a029a71d3f4541aeb5ae
qulinpeng <755146647@qq.com>
2014-05-28 17:10:34 +0800

    自定义分页标签的实现方法基本完成

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 style
 =====
-框架使用spring+spring mvc+hibernate jpa
-主要内容（持续更新。。。）：
+一、框架使用：
+1、后台框架：使用spring+spring mvc+hibernate jpa。
+2、前台框架：使用Boots V2。
+二、主要内容（持续更新。。。）：
 1、对spring data jpa做二次封装生成公共Dao。
+2、自定义jsp标签。
--- src/main/java/com/qlp/commons/tags/PageTag.java
+++ src/main/java/com/qlp/commons/tags/PageTag.java
@@ -1,8 +1,13 @@
 package com.qlp.commons.tags;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
 
 /**
  * Created by qlp on 2014/5/27.
@@ -10,17 +15,18 @@ import javax.servlet.jsp.tagext.TagSupport;
  */
 public class PageTag extends TagSupport {
 
-    private Page<Object> page;
-    private StringBuilder html;
-    private String formId;
-    private String formAction;
+    private static Logger logger = LoggerFactory.getLogger(PageTag.class);
+    private Page<Object> page;  //分页对象
+    private StringBuilder html;  //输出的HTML内容
+    private String formId;  //表单id
+    private String style = "styleOne";  //分页样式，默认为样式1，如果需要其他样式可自定义
 
-    public String getFormAction() {
-        return formAction;
+    public String getStyle() {
+        return style;
     }
 
-    public void setFormAction(String formAction) {
-        this.formAction = formAction;
+    public void setStyle(String style) {
+        this.style = style;
     }
 
     public Page<Object> getPage() {
@@ -37,5 +43,97 @@ public class PageTag extends TagSupport {
 
     public void setFormId(String formId) {
         this.formId = formId;
+    }
+
+    public int doEndTag() throws JspException {
+        this.html = new StringBuilder();
+        if (this.page == null) {
+            return super.EVAL_PAGE;
+        }
+        this.html.append("<ul>").append("\n");
+        if (StringUtils.equals(this.style, "styleOne")) {
+            this.html.append(styleOne());
+        }
+        try {
+            this.pageContext.getOut().write(this.html.toString());  //输出HTML内容到jsp页面
+        } catch (IOException e) {
+            logger.debug("分页标签出错了");
+            throw new JspException();
+        }
+        return super.EVAL_PAGE;//让服务器继续执行jsp页面
+    }
+
+    private StringBuilder styleOne() {
+        StringBuilder sb = new StringBuilder();
+        int currentPage = this.page.getNumber();
+        //如果总页数只有1页
+        if (this.page.getTotalPages() == 1) {
+            sb.append("<li class=\"disabled\">");
+            sb.append("<a href=\"#\">").append(1).append("</a>");
+            sb.append("</li>").append("\n");
+        }
+        //如果总页数大于1页
+        else {
+            //如果当前页是第一页
+            if (this.page.isFirstPage()) {
+                sb.append("<li class=\"disabled\">");
+                sb.append("<a href=\"#\">").append("上一页").append("</a>");
+                sb.append("</li>").append("\n");
+                sb.append(styleOneLess(currentPage));
+                sb.append("<li>");
+                sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                        .append(currentPage + 1).append(");\">").append("下一页").append("</a>");
+                sb.append("</li>").append("\n");
+            }
+            //如果当前页是最后一页
+            else if (this.page.isLastPage()) {
+                sb.append("<li>");
+                sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                        .append(currentPage - 1).append(");\">").append("上一页").append("</a>");
+                sb.append("</li>").append("\n");
+                sb.append(styleOneLess(currentPage));
+                sb.append("<li class=\"disabled\">");
+                sb.append("<a href=\"#\">").append("下一页").append("</a>");
+                sb.append("</li>").append("\n");
+            }
+            //当前页不是第一页也不是最后一页
+            else {
+                sb.append("<li>");
+                sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                        .append(currentPage - 1).append(");\">").append("上一页").append("</a>");
+                sb.append("</li>").append("\n");
+                sb.append(styleOneLess(currentPage));
+                sb.append("<li>");
+                sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                        .append(currentPage + 1).append(");\">").append("下一页").append("</a>");
+                sb.append("</li>").append("\n");
+            }
+        }
+        return sb;
+    }
+
+    private StringBuilder styleOneLess(int currentPage) {
+        StringBuilder sbLess = new StringBuilder();
+        //总页数大于等于1小于10
+        if (this.page.getTotalPages() < 10) {
+            for (int i = 1; i < this.page.getTotalPages(); i++) {
+                if (i == currentPage + 1) {
+                    sbLess.append("<li class=\"active\">");
+                    sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                            .append(i).append(");\">").append(i).append("</a>");
+                    sbLess.append("</li>").append("\n");
+                } else {
+                    sbLess.append("<li>");
+                    sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                            .append(i).append(");\">").append(i).append("</a>");
+                    sbLess.append("</li>").append("\n");
+                }
+            }
+        }
+        //总页数大于10页
+        else {
+
+        }
+        return sbLess;
     }
 }
