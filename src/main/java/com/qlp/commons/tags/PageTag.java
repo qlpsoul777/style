6a10b3d0f9946447b197c17a3f38204d0c981d80
qulinpeng <755146647@qq.com>
2014-05-29 16:39:26 +0800

    完善分页逻辑

--- src/main/java/com/qlp/commons/tags/PageTag.java
+++ src/main/java/com/qlp/commons/tags/PageTag.java
@@ -47,7 +47,7 @@ public class PageTag extends TagSupport {
 
     public int doEndTag() throws JspException {
         this.html = new StringBuilder();
-        if (this.page == null) {
+        if (this.page.getContent().isEmpty()) {
             return super.EVAL_PAGE;
         }
         this.html.append("<ul>").append("\n");
@@ -82,14 +82,14 @@ public class PageTag extends TagSupport {
                 sb.append(styleOneLess(currentPage));
                 sb.append("<li>");
                 sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
-                        .append(currentPage + 1).append(");\">").append("下一页").append("</a>");
+                        .append(currentPage + 2).append(");\">").append("下一页").append("</a>");
                 sb.append("</li>").append("\n");
             }
             //如果当前页是最后一页
             else if (this.page.isLastPage()) {
                 sb.append("<li>");
                 sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
-                        .append(currentPage - 1).append(");\">").append("上一页").append("</a>");
+                        .append(currentPage).append(");\">").append("上一页").append("</a>");
                 sb.append("</li>").append("\n");
                 sb.append(styleOneLess(currentPage));
                 sb.append("<li class=\"disabled\">");
@@ -100,23 +100,36 @@ public class PageTag extends TagSupport {
             else {
                 sb.append("<li>");
                 sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
-                        .append(currentPage - 1).append(");\">").append("上一页").append("</a>");
+                        .append(currentPage).append(");\">").append("上一页").append("</a>");
                 sb.append("</li>").append("\n");
                 sb.append(styleOneLess(currentPage));
                 sb.append("<li>");
                 sb.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
-                        .append(currentPage + 1).append(");\">").append("下一页").append("</a>");
+                        .append(currentPage + 2).append(");\">").append("下一页").append("</a>");
                 sb.append("</li>").append("\n");
             }
         }
+        sb.append("<li><input type=\"hidden\" id=\"").append(this.formId)
+                .append("_page_id\" name=\"page.page\" value=\"").append(this.page.getNumber() + 1)
+                .append("\"></li>").append("\n");
+        sb.append("<script type=\"text/javascript\">").append("\n");
+        sb.append("function toPage(formId, page) {").append("\n");
+        sb.append("var $form = $('#' + formId);").append("\n");
+        sb.append("reg = /^[0-9]*$/;").append("\n");
+        sb.append("if (reg.exec(page + \"\")) {").append("\n");
+        sb.append("$(\"#\" + formId + \"_page_id\").val(page);").append("\n");
+        sb.append("}").append("\n");
+        sb.append("$form[0].submit();").append("\n");
+        sb.append("}").append("\n");
+        sb.append("</script>").append("\n");
         return sb;
     }
 
     private StringBuilder styleOneLess(int currentPage) {
         StringBuilder sbLess = new StringBuilder();
         //总页数大于等于1小于10
-        if (this.page.getTotalPages() < 10) {
-            for (int i = 1; i < this.page.getTotalPages(); i++) {
+        if (this.page.getTotalPages() <= 10) {
+            for (int i = 1; i <= this.page.getTotalPages(); i++) {
                 if (i == currentPage + 1) {
                     sbLess.append("<li class=\"active\">");
                     sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
@@ -132,6 +145,54 @@ public class PageTag extends TagSupport {
         }
         //总页数大于10页
         else {
+            int onePageShow = 10;  //一页显示的页数
+            int half = onePageShow >> 1;
+            int halfPageShow = half + 1;
+            int havePages = this.page.getTotalPages() - currentPage;  //总页数减去当前页
+            int stopPage = this.page.getTotalPages() - onePageShow;  //总页数减去一页显示的页数
+            if (currentPage <= halfPageShow) {
+                for (int i = 1; i <= onePageShow; i++) {
+                    if (i == currentPage + 1) {
+                        sbLess.append("<li class=\"active\">");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    } else {
+                        sbLess.append("<li>");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    }
+                }
+            } else if (havePages >= 4) {
+                for (int i = currentPage - 5; i < currentPage + 5; i++) {
+                    if (i == currentPage + 1) {
+                        sbLess.append("<li class=\"active\">");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    } else {
+                        sbLess.append("<li>");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    }
+                }
+            } else {
+                for (int i = stopPage; i <= this.page.getTotalPages(); i++) {
+                    if (i == currentPage + 1) {
+                        sbLess.append("<li class=\"active\">");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    } else {
+                        sbLess.append("<li>");
+                        sbLess.append("<a href=\"javascript:toPage('").append(this.formId).append("', ")
+                                .append(i).append(");\">").append(i).append("</a>");
+                        sbLess.append("</li>").append("\n");
+                    }
+                }
+            }
         }
         return sbLess;
     }
