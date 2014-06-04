8f83d4ec069c54598dfc3d6693cf766c4ca2741d
qulinpeng <755146647@qq.com>
2014-06-04 17:52:33 +0800

    增加中间体

---
+++ src/main/java/com/qlp/user/dto/Application_.java
@@ -0,0 +1,25 @@
+package com.qlp.user.dto;
+
+/**
+ * Created by wang on 2014/6/4.
+ */
+public class Application_ {
+    private String aId;
+    private String aName;
+
+    public String getaId() {
+        return aId;
+    }
+
+    public void setaId(String aId) {
+        this.aId = aId;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName;
+    }
+}
